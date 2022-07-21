078515010a0931df0fa6f640112559ea611672cf
Yunha <ha110011@naver.com>
2022-07-21 12:27:51 +0900

    Style: [FE] 서비스 파비콘 업로드 및 Vuetify 적용
    Vuetify 적용하여 기존 네비게이션 바 수정했습니다.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <v-app id="faffy">
     <Header />
-    <!-- <profile-view /> -->
-    <router-view></router-view>?
+    <auth-view />
+    <!--<router-view></router-view>-->
     <Footer />
   </v-app>
 </template>
 
 <script>
 import Header from './components/common/Header.vue'
-// import ProfileView from './views/ProfileView.vue'
+//import ProfileView from './views/ProfileView.vue'
+import AuthView from './views/AuthView.vue'
 import Footer from './components/common/Footer.vue'
 
 export default {
@@ -17,7 +18,7 @@ export default {
   components: {
     Header,
     Footer,
-    // ProfileView
+    AuthView
   }
 }
 </script>
@@ -27,8 +28,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  width: calc(100vw - (100vw - 100%))px;
+  width: calc(100vw - (100vw - 100%));
+  height: 100vh;
   text-align: center;
-  background-color: #2c3e50;
+  background-color: #afb2b5;
 }
 </style>
