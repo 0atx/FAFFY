8b289e96048d8d0759d7164b72dc40080f182b8c
Yunha <ha110011@naver.com>
2022-07-22 12:46:44 +0900

    Feat: [FE] 프로필 페이지 UI 작업
    프로필 페이지 UI 작업 및 이후 작업할 페이지 생성했습니다.

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="container">
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HomeView",
+
+	data() {
+		return {};
+	},
+	mounted() {},
+
+	methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid>
+    <v-row wrap>
+      <v-col cols="4">
+        <profile-card />
+      </v-col>
+      <v-col cols="8">
+      <div></div>
+      <!--<router-view></router-view>-->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ProfileCard from '../components/user/ProfileCard.vue'
+
+export default {
+	name: "ProfileView",
+  components: {
+    ProfileCard
+  }
+};
+</script>
+
+<style scoped>
+* {
+  height: 100%;
+  padding: 0px;
+}
+
+v-container {
+  width: 100%;
+}
+
+div {
+  background-color: gray;
+}
+</style>
