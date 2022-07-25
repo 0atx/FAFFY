39177f69de28219f550a124f15281fadad604b4d
Yunha <ha110011@naver.com>
2022-07-25 17:49:48 +0900

    Feat: [FE] 회원가입 이용약관 페이지 추가

---
+++ frontend/src/components/auth/ServiceTermsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+<strong style="font-size:20px">요 약</strong>
+임시입니다.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ServiceTerms",
+}
+</script>
+
+<style>
+
+</style>
