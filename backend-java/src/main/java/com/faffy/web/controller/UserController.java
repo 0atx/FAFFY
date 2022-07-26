f9905bfee39b6447bb04aa0f14aa7ae27868369f
Jun <js727r@gmail.com>
2022-07-26 13:42:59 +0900

    Feat: Add FE sign up
    프론트에서 회원가입 요청하는 기능을 구현하였습니다.

--- backend-java/src/main/java/com/faffy/web/controller/UserController.java
+++ backend-java/src/main/java/com/faffy/web/controller/UserController.java
@@ -12,6 +12,8 @@ import com.faffy.web.service.UserServiceImpl;
 import com.faffy.web.service.token.JwtTokenProvider;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +34,7 @@ public class UserController {
     UserServiceImpl userService;
 
     private final JwtTokenProvider jwtTokenProvider;
+    public static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     /**
      * 모든 회원 정보 받아오기
@@ -54,9 +57,10 @@ public class UserController {
     @ApiOperation(value="회원 가입",notes="유저 정보를 받아 db에 유저를 추가합니다.")
     @PostMapping
     public ResponseEntity addUser(@Valid @RequestBody UserDto userDto) throws DataIntegrityException, IllegalInputException {
+        logger.info("dto : {}",userDto);
         User user = userService.addUser(userDto);
         HashMap<String, Object> hashmap = new HashMap<>();
-        hashmap.put("content",user);
+        hashmap.put("content",user.toPublicDto());
         return ResponseEntity.ok().body(hashmap);
     }
 
