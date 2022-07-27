a444d93615ee2f6ca0729a6c6ea0c7c83808c181
Yunha <ha110011@naver.com>
2022-07-27 11:18:23 +0900

    Feat: [FE] 프로필 좌측 컴포넌트 레이아웃 완성
    프로필 페이지 좌측에 들어가는 컴포넌트 레이아웃 완성했습니다. 추후 백엔드에서 넘어오는 데이터와 연동 작업 필요합니다.

--- frontend/src/components/user/ProfileCard.vue
+++ frontend/src/components/user/ProfileCard.vue
@@ -3,43 +3,168 @@
 프로필 뷰 - 좌측 프로필 정보
 -->
 <template>
-    <v-row id="profileCard" no-gutters>
-      <v-col>
-        <v-avatar
-          class="profile"
-          size="164"
-          tile
-        >
-          <v-img src="../../assets/images/faffy_logo.png"></v-img>
-        </v-avatar>
-      </v-col>
-      <v-col>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            이름
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            팔로잉 팔로워
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-col>
-    </v-row>
+  <div id="profileCard">
+
+    <!-- 프로필 이미지
+      아래 이미지 v-if로 사진 없을 때 디폴트 사진을 보여주던가 해야할거 같음
+    -->
+    <v-avatar
+      color="#fff"
+      class="mt-8 mb-4"
+      size="250"
+      rounded
+    >
+
+      <v-img src="@/assets/images/default_profile.png"></v-img>
+      <!--
+      <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+      -->
+    </v-avatar>
+
+    <!-- 유저 프로필 정보 -->
+    <div id="profile">
+      <div>
+        <!-- 별명 -->
+        <div id="name" class="text-h6 mb-2">
+          별명짓기귀찮다
+        </div>
+
+        <!-- 팔로잉 팔로워 -->
+        <div>
+          <router-link to="/auth/sign-up">팔로잉 163</router-link>
+          <router-link to="/auth/sign-up">팔로워 208</router-link>
+        </div>
+
+        <!-- 한 줄 자기소개 -->
+        <div id="introduce">
+          한 줄 소개 들어갈 부분입니다. 근데 한 줄 소개는 글자수 제한을 몇자로 하지 100자? 200자? 이거도 의논해야 함
+        </div>
+
+        <!-- 관심 카테고리 -->
+        <div id="category" class="mb-2">
+          <v-chip-group
+            column
+          >
+            <category-chips
+              v-for="category in categorys"
+              :key="category"
+              :category="category"
+            />
+          </v-chip-group>
+        </div>
+
+        <!-- SNS 정보
+          v-if로 없으면 보이게 안 보이게 설정하고 있으면 버튼에 링크 달아줘야 함
+        -->
+        <div id="social" class="mb-2">
+
+          <!-- 인스타그램 -->
+          <v-btn
+            fab
+            elevation="0"
+            class="overflow-hidden"
+          >
+            <img
+              src="@/assets/images/instagram_logo.png"
+              alt=""
+              style="width:60px; height:60px;"
+            >
+          </v-btn>
+
+          <!-- 페이스북 -->
+          <v-btn
+            fab
+            elevation="0"
+            class="overflow-hidden"
+          >
+            <img
+              src="@/assets/images/facebook_logo.png"
+              alt=""
+              style="width:60px; height:60px;"
+            >
+          </v-btn>
+
+          <!-- 유튜브 -->
+          <v-btn
+            fab
+            elevation="0"
+            class="overflow-hidden"
+          >
+            <img
+              src="@/assets/images/youtube_logo.png"
+              alt=""
+              style="width:60px; height:60px;"
+            >
+          </v-btn>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import CategoryChips from '@/components/common/CategoryChips.vue'
+
 export default {
   name: "ProfileCard",
+  components: { CategoryChips },
   data() {
-      return {
-
-      }
-  }
+    return {
+      categorys: ["워크웨어", "히피", "페미닌", "캐주얼", "모던", "시크", "댄디", "빈티지", "미니멀", "스트릿" ],
+      readonly: { type: Boolean, default: true },
+    }
+  },
+  metaInfo () {
+    return {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
 };
 </script>
 
 <style scoped>
 #profileCard {
-  background-color: blueviolet;
-  float: left;
+  background-color: #8b8fda9f;
+  padding: 5%;
+  position:sticky;
+}
+
+#profile {
+  width: 80%;
+  margin: auto;
+}
+
+#introduce {
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#category {
+  height: 140px;
+}
+
+#info {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+#name {
+  font-weight: 600;
+}
+
+a {
+  color: black;
+  margin: 30px;
+  text-decoration: none;
+}
+
+#social {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
