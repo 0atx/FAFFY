ac32ae53f9ad488a9a5a9f8bd391ce898941d4d7
Yunha <ha110011@naver.com>
2022-07-27 18:22:35 +0900

    Feat: [FE] 프로필 상세 페이지 및 라우터 추가
    프로필 수정 페이지 추가, 게시글 목록 페이지 추가, 방송 기록 페이지 추가, 라우터 설정했습니다.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,82 +4,86 @@ import MainView from "@/views/MainView.vue";
 
 Vue.use(VueRouter);
 
-
 const routes = [
   {
-    path:"/",
-    name:"main",
-    component:MainView
+    path: "/",
+    name: "main",
+    component: MainView,
   },
   {
-    path:"/auth",
-    name:"auth",
-    redirect:"/auth/sign-in",
-    component:()=>import("@/views/AuthView.vue"),
+    path: "/auth",
+    name: "auth",
+    redirect: "/auth/sign-in",
+    component: () => import("@/views/AuthView.vue"),
     children: [
       {
-        path:"sign-in",
-        name:"sign-in",
-        component:()=>import("@/components/auth/SignInView.vue")
+        path: "sign-in",
+        name: "sign-in",
+        component: () => import("@/components/auth/SignInView.vue"),
       },
       {
-        path:"sign-up",
-        name:"sign-up",
-        component:()=>import("@/components/auth/SignUpView.vue")
+        path: "sign-up",
+        name: "sign-up",
+        component: () => import("@/components/auth/SignUpView.vue"),
       },
       {
-        path:"service-terms",
-        name:"service-terms",
-        component:()=>import("@/components/auth/ServiceTermsView.vue")
+        path: "service-terms",
+        name: "service-terms",
+        component: () => import("@/components/auth/ServiceTermsView.vue"),
       },
       {
-        path:"private-terms",
-        name:"private-terms",
-        component:()=>import("@/components/auth/PrivateTermsView.vue")
-      }
-    ]
+        path: "private-terms",
+        name: "private-terms",
+        component: () => import("@/components/auth/PrivateTermsView.vue"),
+      },
+    ],
   },
   {
-    path:"/profile/:id",
-    name:"profile",
-    component:()=>import("@/views/ProfileView.vue"),
+    path: "/profile/:id",
+    name: "profile",
+    component: () => import("@/views/ProfileView.vue"),
     children: [
       {
-        path:"user-detail",
-        name:"user-detail",
-        component:()=>import("@/components/user/ProfileDetailView.vue"),
+        path: "user-detail",
+        name: "user-detail",
+        component: () => import("@/components/user/ProfileDetailView.vue"),
       },
       {
-        path:"history",
-        name:"user-history",
-        component:()=>import("@/components/user/HistoryView.vue"),
+        path: "history",
+        name: "user-history",
+        component: () => import("@/components/user/HistoryView.vue"),
       },
       {
-        path:"history-detail",
-        name:"user-history-detail",
-        component:()=>import("@/components/user/HistoryDetailView.vue"),
-      }
-    ]
-  },
-  {
-    path:"/edit-profile",
-    name:"edit-profile",
-    component:()=>import("@/views/EditProfileView.vue"),
-  },
-  {
-    path:"/consulting",
-    name:"consulting",
-    redirect:"/consulting/ready",
-    component:()=>import("@/views/ProfileView.vue"),
-    children:[
+        path: "history-detail",
+        name: "user-history-detail",
+        component: () => import("@/components/user/HistoryDetailView.vue"),
+      },
       {
-        path:"ready",
-        name:"consulting-ready",
-        component:()=>import("@/components/user/ProfileDetailView.vue"),
-      }
-    ]
-  }
-]
+        path: "user-board",
+        name: "user-board",
+        component: () => import("@/components/user/BoardView.vue"),
+      },
+    ],
+  },
+  {
+    path: "/edit-profile",
+    name: "edit-profile",
+    component: () => import("@/views/EditProfileView.vue"),
+  },
+  {
+    path: "/consulting",
+    name: "consulting",
+    redirect: "/consulting/ready",
+    component: () => import("@/views/ProfileView.vue"),
+    children: [
+      {
+        path: "ready",
+        name: "consulting-ready",
+        component: () => import("@/components/user/ProfileDetailView.vue"),
+      },
+    ],
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
