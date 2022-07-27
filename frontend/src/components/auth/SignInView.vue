55c011fcdc0840e9e78ef375a3e939c80dce4319
Yunha <ha110011@naver.com>
2022-07-27 18:21:32 +0900

    Style: [FE] 페이지 height 재설정 및 코드 포맷팅
    페이지 height 재설정 했습니다.

--- frontend/src/components/auth/SignInView.vue
+++ frontend/src/components/auth/SignInView.vue
@@ -4,16 +4,8 @@
 -->
 <template>
   <v-container>
-    <img
-      class="mt-10"
-      src="@/assets/images/faffy_logo_big.png"
-      alt="faffy logo"
-    >
-    <v-form
-      ref="form"
-      id="signIn"
-      @submit.prevent="requestSignIn"
-    >
+    <v-form ref="form" id="signIn" @submit.prevent="requestSignIn">
+      <img src="@/assets/images/faffy_logo_big.png" alt="faffy logo" />
       <!-- 이메일 입력 -->
       <v-text-field
         v-model="form.email"
@@ -21,7 +13,7 @@
         label="이메일"
         required
         @keydown.enter="onInputKeyword"
-        />
+      />
 
       <!-- 비밀번호 입력 -->
       <v-text-field
@@ -34,55 +26,34 @@
         @keydown.enter="onInputKeyword"
       />
 
-      <dark-button
-        :btnValue="signInValue"
-        @click="requestSignIn"
-      />
+      <dark-button :btnValue="signInValue" @click="requestSignIn" />
 
       <!-- 회원가입 및 비밀번호 찾기  -->
       <div id="route" class="mt-4">
-        <div
-          type="button"
-          @click="goTo"
-        >
-          회원가입
-        </div>
-        <div
-          type="button"
-          @click="findPassword"
-        >
-          비밀번호 찾기
-        </div>
+        <div type="button" @click="goTo">회원가입</div>
+        <div type="button" @click="findPassword">비밀번호 찾기</div>
       </div>
 
       <!-- 소셜 로그인 -->
-      <hr>
+      <hr />
 
       <div id="social">
         <!-- 네이버 로그인 -->
-        <v-btn
-          fab
-          elevation="0"
-          class="overflow-hidden"
-        >
+        <v-btn fab elevation="0" class="overflow-hidden">
           <img
             src="@/assets/images/naver_login.png"
             alt=""
-            style="width:60px; height:60px;"
-          >
+            style="width: 60px; height: 60px"
+          />
         </v-btn>
 
         <!-- 구글 로그인 -->
-        <v-btn
-          fab
-          elevation="0"
-          class="overflow-hidden"
-        >
+        <v-btn fab elevation="0" class="overflow-hidden">
           <img
             src="@/assets/images/google_login.png"
             alt=""
-            style="width:80px; height:80px;"
-          >
+            style="width: 80px; height: 80px"
+          />
         </v-btn>
       </div>
     </v-form>
@@ -90,62 +61,66 @@
 </template>
 
 <script>
-import DarkButton from '@/components/common/DarkButton.vue'
+import DarkButton from "@/components/common/DarkButton.vue";
 import { mapMutations } from "vuex";
 import { auth } from "@/api/auth.js";
-const authStore ="authStore";
+const authStore = "authStore";
 
 export default {
   name: "SignIn",
   components: {
-    DarkButton
+    DarkButton,
   },
   data() {
-      return {
-        form: {
-          email: '',
-          password: '',
-        },
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
 
-        type: false,
+      type: false,
 
-        signInValue: '로그인'
-      }
+      signInValue: "로그인",
+    };
   },
-  computed:{
-  },
+  computed: {},
   methods: {
-    ...mapMutations(authStore,["SET_IS_LOGIN","SET_USER_INFO","SET_IS_LOGIN_ERROR"]),
+    ...mapMutations(authStore, [
+      "SET_IS_LOGIN",
+      "SET_USER_INFO",
+      "SET_IS_LOGIN_ERROR",
+    ]),
     async save() {
-    this.$refs.form.validate();
-    await this.$nextTick();
-    if(!this.valid) return;
-    console.log(this.form);
+      this.$refs.form.validate();
+      await this.$nextTick();
+      if (!this.valid) return;
+      console.log(this.form);
     },
     goTo() {
-      this.$router.push({ name: "sign-up" })
+      this.$router.push({ name: "sign-up" });
     },
     async requestSignIn() {
-      auth.login(this.form,
-      ()=>{
-        this.$router.push({ name: "main" });
-      },
-      ()=>{
-        alert("로그인 실패. 나중에 바꿔야함");
-      })
-
+      auth.login(
+        this.form,
+        () => {
+          this.$router.push({ name: "main" });
+        },
+        () => {
+          alert("로그인 실패. 나중에 바꿔야함");
+        }
+      );
     },
     findPassword() {
       console.log("모달 나와야합니다.");
-    }
+    },
   },
-  metaInfo () {
+  metaInfo() {
     return {
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-      ]
-    }
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    };
   },
 };
 </script>
@@ -154,6 +129,10 @@ export default {
 .container {
   background-color: white;
   padding: 5%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 #signIn {
@@ -175,7 +154,7 @@ hr {
 }
 
 hr:after {
-  content: '소셜 로그인';
+  content: "소셜 로그인";
   display: inline-block;
   position: relative;
   top: -13px;
--- frontend/src/components/user/ProfileDetailView.vue
+++ frontend/src/components/user/ProfileDetailView.vue
@@ -4,41 +4,62 @@
 -->
 <template>
   <div id="profileDetail">
-    <div id="info" class="mt-4">
-      <p class="text-h6" style="font-weight: 600">정보</p>
-      <hr>
-    </div>
-    <div id="history">
-      <p class="text-h6" style="font-weight: 600">방송 기록
-        <v-btn
-          class="mx-2 pb-1"
-          elevation="0"
-          icon
-          small
-          color="gray"
-        >
-          <v-icon>
-            mdi-plus
-          </v-icon>
-        </v-btn>
-      </p>
-      <hr>
-    </div>
-    <div id="board">
-      <p class="text-h6" style="font-weight: 600">게시글
-        <v-btn
-          class="mx-2 pb-1"
-          elevation="0"
-          icon
-          small
-          color="gray"
-        >
-          <v-icon>
-            mdi-chevron-right
-          </v-icon>
-        </v-btn>
-      </p>
-      <hr>
+    <div>
+      <div id="route">
+        <p class="text-h6" style="font-weight: 600">Profile</p>
+      </div>
+      <div id="content" class="mt-4">
+        <p class="text-h6" style="font-weight: 600">정보</p>
+        <hr />
+        <div id="info">
+          긴 글 소개 부분입니다. 개인 정보 수정은 왼쪽 프로필 카드에서 할 수
+          있습니다. 생년월일이나 성별에 대한 정보들은 보여지지 않습니다.
+        </div>
+      </div>
+      <div id="content">
+        <p class="text-h6" style="font-weight: 600">
+          방송 기록
+          <v-btn
+            id="moreBtn"
+            class="mx-2 pb-1"
+            elevation="0"
+            icon
+            small
+            color="gray"
+            to="/profile/1/history"
+          >
+            <v-icon> mdi-plus </v-icon>
+          </v-btn>
+        </p>
+        <hr />
+        <div id="history">
+          방송 기록 리스트 부분입니다. 추후 방송 기록 리스트 컴포넌트 구현 후
+          추가할 예정입니다. <br />ex) 카테고리(참여, 진행) | 방송 제목 ㅁ사진
+          갯수 or 방송 일자
+        </div>
+      </div>
+      <div id="content">
+        <p class="text-h6" style="font-weight: 600">
+          게시글
+          <v-btn
+            id="moreBtn"
+            class="mx-2 pb-1"
+            elevation="0"
+            icon
+            small
+            color="gray"
+            to="/profile/1/user-board"
+          >
+            <v-icon> mdi-plus </v-icon>
+          </v-btn>
+        </p>
+        <hr />
+        <div id="board">
+          방송 기록 리스트 부분입니다. 추후 방송 기록 리스트 컴포넌트 구현 후
+          추가할 예정입니다. <br />ex) 카테고리(Q&A, 자유) | 게시글 제목 ㅁ댓글
+          갯수 or 작성 일자
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -47,41 +68,58 @@
 export default {
   name: "ProfileDetailView",
   data() {
-      return {
-
-      }
+    return {};
   },
-  metaInfo () {
+  metaInfo() {
     return {
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-      ]
-    }
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    };
   },
 };
 </script>
 
 <style scoped>
 #profileDetail {
-  background-color: rgb(218, 77, 77);
+  background-color: #fff;
   padding: 5%;
   text-align: left;
+  height: 100%;
+  display: flex;
+  justify-content: start;
+  align-items: flex-start;
+  cursor: default;
 }
 
 #profileDetail div {
+  width: 100%;
+}
+
+#content {
   height: 300px;
+  background-color: #fff;
+  padding: 2%;
 }
 
 #info {
-
+  height: 200px;
 }
 
 #history {
-
+  height: 300px;
 }
 
 #board {
+  height: 300px;
+}
 
+#moreBtn::before {
+  background-color: transparent;
+}
+
+#moreBtn i:hover {
+  transform: scale(1.15);
 }
 </style>
