d5bef0459a73632666f4b41bfbda963bb88d2841
yongwoo <woojaang@naver.com>
2022-07-28 10:01:56 +0900

    Feat: S07P12A802-119 게시판 인증 구현 게시판 글 작성, 수정, 삭제에 인증을 추가했습니다.

--- backend-java/src/main/java/com/faffy/web/service/BoardService.java
+++ backend-java/src/main/java/com/faffy/web/service/BoardService.java
@@ -22,21 +22,21 @@ public interface BoardService {
      * @param boardDto 게시글 내용을 담은 객체
      * @throws Exception 글쓴이 번호에 해당하는 유저를 찾을 수 없거나 입력값이 잘못된 경우 예외 발생
      */
-    public void writeBoard(BoardDto boardDto) throws Exception;
+    public void writeBoard(BoardDto boardDto, int user_no) throws Exception;
 
     /**
      * 게시글 수정
      * @param boardUpdateDto 수정할 게시글의 내용을 담은 객체
      * @throws Exception 해당 게시글이 없거나 유효하지 않은 입력 시 예외 발생
      */
-    public void updateBoard(BoardUpdateDto boardUpdateDto) throws Exception;
+    public void updateBoard(BoardUpdateDto boardUpdateDto, int user_no) throws Exception;
 
     /**
      * 게시글 삭제
      * @param no 삭제하려는 게시글 번호
      * @throws Exception 해당 게시글이 없을경우 예외 발생
      */
-    public void deleteBoard(int no) throws Exception;
+    public void deleteBoard(int no, int user_no) throws Exception;
 
     /**
      * 게시글 번호에 해당하는 게시글 상세보기
