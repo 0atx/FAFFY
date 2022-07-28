e347293103ae1f0dcba7a15ecc63f1ee44e77e9c
Jun <js727r@gmail.com>
2022-07-28 09:33:39 +0900

    Feat: Add user category
    유저 카테고리 추가 / 삭제 기능을 구현했습니다.

--- backend-java/src/main/java/com/faffy/web/exception/ExceptionMsg.java
+++ backend-java/src/main/java/com/faffy/web/exception/ExceptionMsg.java
@@ -7,4 +7,5 @@ public class ExceptionMsg {
     static public final String ILLEGAL_INPUT_MSG = "입력 값을 확인해 주세요.";
     static public final String BOARD_NOT_FOUND_MSG = "게시글을 찾을 수 없습니다.";
     static public final String COMMENT_NOT_FOUND_MSG = "댓글을 찾을 수 없습니다.";
+    static public final String CATEGORY_NOT_FOUND_MSG = "등록되지 않은 카테고리입니다.";
 }
