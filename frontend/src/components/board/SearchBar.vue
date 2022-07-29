b4e008a591d340248edcdf34601eb646e8b1c25c
kyong0409 <kyong0409@nate.com>
2022-07-29 16:29:50 +0900

    Feat: [FE] 게시판 검색창 기본 설정
    게시판 검색창 기본 설정입니다

--- frontend/src/components/board/SearchBar.vue
+++ frontend/src/components/board/SearchBar.vue
@@ -4,12 +4,42 @@
 최종 수정일: 2022.07.29
 -->
 <template>
-  <h3>search bar</h3>
+  <v-container fluid>
+    <v-row align="center">
+      <!--검색 기준 선택(작성자 or 제목)-->
+      <v-col
+        class="d-flex"
+        cols="2"
+      >
+        <v-select
+          :items="items"
+          label="검색"
+          outlined
+        ></v-select>
+      </v-col>
+
+      <!--검색어 입력-->
+      <v-col
+          cols="8"
+        >
+          <v-text-field
+            v-model="keyword"
+            label="검색어를 입력하세요"
+            append-icon="mdi-magnify"
+          ></v-text-field>
+        </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      items: ['작성자', '제목'],
+      keyword: ''
+    }
+  }
 }
 </script>
 
