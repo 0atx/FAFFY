7a7b28bd11a815a9be5fefbaec56c0a562220ec5
kyong0409 <kyong0409@nate.com>
2022-07-29 15:06:58 +0900

    Feat: [FE] 게시글 목록 보기
    게시글 목록 화면 테이블 형식으로 설정했습니다.

---
+++ frontend/src/components/board/ArticleList.vue
@@ -0,0 +1,78 @@
+<!--
+작성자: 류경하
+설명: 게시판 글 목록
+최종 수정일: 2022.07.29
+-->
+<template>
+  <div>
+    <h3>article list</h3>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">
+              작성자
+            </th>
+            <!--추후에 카테고리 대신 댓글 수로 대체 예정-->
+            <th class="text-center">
+              카테고리
+            </th>
+            <th class="text-center">
+              제목
+            </th>
+            <th class="text-center">
+              작성일
+            </th>
+          </tr>
+        </thead>
+        <!--각각의 게시글, 클릭하면 상세조회로 이동-->
+        <tbody>
+          <tr
+            v-for="article in articleList"
+            :key="article.articleNo"
+            @click="articleDetail(article)"
+            style="cursor: pointer"
+          >
+            <td>{{ article.writer }}</td>
+            <td>{{ article.category }}</td>
+            <td>{{ article.title }}</td>
+            <td>{{ article.created_at }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-pagination
+      v-model="page"
+      :length="4"
+      prev-icon="mdi-menu-left"
+      next-icon="mdi-menu-right">
+    </v-pagination>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+const boardStore = "boardStore"
+
+export default {
+  name: 'ArticleList',
+  data () {
+      return {
+        page: 1
+      }
+    },
+  computed: {
+    ...mapGetters(boardStore, ['articleList']),
+  },
+  methods: {
+    // 상세조회 페이지 이동(추후 수정)
+    articleDetail(article) {
+      console.log(`제목: ${article.title} 글로 이동`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/board/SideMenu.vue
@@ -0,0 +1,18 @@
+<!--
+작성자: 류경하
+설명: 게시판 좌측 메뉴(질문응답/자유/후기 게시판)
+최종 수정일: 2022.07.29
+-->
+<template>
+  <h2>side bar</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
