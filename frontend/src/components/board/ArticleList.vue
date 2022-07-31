a25a08c08fb7423c6a9a9440b0742574bdaee169
kyong0409 <kyong0409@nate.com>
2022-07-31 14:18:56 +0900

    Feat: [FE] 게시글 목록 pagination 구현
    게시글 목록 Paginaton 기능입니다.

--- frontend/src/components/board/ArticleList.vue
+++ frontend/src/components/board/ArticleList.vue
@@ -28,7 +28,7 @@
         <!--각각의 게시글, 클릭하면 상세조회로 이동-->
         <tbody>
           <tr
-            v-for="article in articleList"
+            v-for="article in currentPage"
             :key="article.articleNo"
             @click="articleDetail(article)"
             style="cursor: pointer"
@@ -43,9 +43,10 @@
     </v-simple-table>
     <v-pagination
       v-model="page"
-      :length="4"
+      :length="totalPages"
       prev-icon="mdi-menu-left"
-      next-icon="mdi-menu-right">
+      next-icon="mdi-menu-right"
+      :total-visible="totalVisible">
     </v-pagination>
   </div>
 </template>
@@ -58,11 +59,21 @@ export default {
   name: 'ArticleList',
   data () {
       return {
-        page: 1
+        page: 1,
+        itemsPerPage: 2,
+        totalVisible: 7,
       }
     },
   computed: {
     ...mapGetters(boardStore, ['articleList']),
+    totalPages() {
+      return this.articleList.length % this.itemsPerPage > 0 ? parseInt(this.articleList.length/this.itemsPerPage)+1 : parseInt(this.articleList.length/this.itemsPerPage)
+    },
+    currentPage() {
+      const start = (this.page-1)*this.itemsPerPage
+      const end = start+this.itemsPerPage
+      return this.articleList.slice(start, end)
+    }
   },
   methods: {
     // 상세조회 페이지 이동(추후 수정)
