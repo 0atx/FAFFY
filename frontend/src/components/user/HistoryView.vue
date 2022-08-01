fa3d84844dc1e3e24bfbc5a91515096b030701ce
Yunha <ha110011@naver.com>
2022-08-01 16:37:32 +0900

    Feat: [FE] 회원 관리 페이지 레이아웃 완성
    게시글 목록 페이지
    팔로워 목록 페이지
    팔로잉 목록 페이지
    방송 기록 페이지
    전체적인 화면 레이아웃 구성 및 CSS 작업 완료 했습니다. 추후 기능 구현 필요합니다.

--- frontend/src/components/user/HistoryView.vue
+++ frontend/src/components/user/HistoryView.vue
@@ -9,29 +9,34 @@
         <p class="text-h6" style="font-weight: 600">History</p>
       </div>
       <div id="content" class="mt-4">
-        <p class="text-h6" style="font-weight: 600">
-          <!--방송 기록 참여 | 진행 버튼 들어올 자리-->
-          <v-btn>방송 참여</v-btn> |
-          <v-btn>방송 진행</v-btn>
+        <p class="text-h6">
+          <!--방송 참여 | 진행 기록 탭-->
+          <v-tabs color="0c0f66">
+            <v-tab :id="selected ? 'selectedBtn' : 'selectBtn'" @click="selected = true">참여 기록</v-tab>
+            <v-tab :id="!selected ? 'selectedBtn' : 'selectBtn'" @click="selected = false">진행 기록</v-tab>
+          </v-tabs>
         </p>
-        <hr />
-        <div id="info">
-          <!--방송 참여 또는 진행 목록 컴포넌트가 들어올 자리입니다.-->
+
+        <div>
+          <!--방송 참여 또는 진행 목록 리스트 selected로 참여인지 진행인지 구분해서 출력할 것 -->
           <v-data-table
+            id="table"
             :headers="headers"
-            hide-default-header
             hide-default-footer
             :items="consults"
             :items-per-page="itemsPerPage"
             :page.sync="page"
             @page-count="pageCount = $event"
           ></v-data-table>
-          <div class="text-center pt-2">
+
+          <!-- pagination -->
+          <div class="text-center pt-4">
             <v-pagination
               circle
               color="#0c0f66"
               v-model="page"
               :length="pageCount"
+              :total-visible="5"
             ></v-pagination>
           </div>
         </div>
@@ -47,85 +52,105 @@ export default {
     return {
       headers: [
         { text: '방송 제목', align:'start', value:'title'},
-        { text: '방송 내용', value:'content'},
+        { text: '방송 소개', value:'info'},
+        { text: '방송 진행자', value:'consultant'},
         { text: '방송 일자', value:'date'},
       ],
+      // 임시 방송 기록, DB에서 받아와서 넘겨줘야 함
       consults: [
         {
           title: '방송 제목입니다.',
-          content: '카테고리 넣고 상세 페이지에는 그 날 입었던 옷들 기록?',
+          info: '카테고리 넣고 상세 페이지에는 그 날 입었던 옷들 기록?',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
-          title: '방송 제목입니다.2',
-          content: '여기 뭐 넣어야 되냐?;;',
+          title: '제목도 10자 제한',
+          info: '30자 출력하게 해야함',
+          consultant: '별명도 10자?',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.3',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: 'if consults.info.length <= 30 그냥 출력',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.4',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: 'else consult.info.substring(0, 30) + "..." 해서 출력',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.5',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.6',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.7',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.8',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.9',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.10',
-          content: '여기 뭐 넣어야 되냐?;;',
-          date: '2022-08-01'
-        },
-        {
-          title: '방송 제목입니다.3',
-          content: '여기 뭐 넣어야 되냐?;;',
-          date: '2022-08-01'
-        },
-        {
-          title: '방송 제목입니다.4',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.11',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
         {
           title: '방송 제목입니다.12',
-          content: '여기 뭐 넣어야 되냐?;;',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
+          date: '2022-08-01'
+        },
+        {
+          title: '방송 제목입니다.13',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
+          date: '2022-08-01'
+        },
+        {
+          title: '방송 제목입니다.14',
+          info: '여기 뭐 넣어야 되냐?;;',
+          consultant: '별명짓기귀찮다',
           date: '2022-08-01'
         },
       ],
 
+      // pagination 관련 변수
       page: 1,
       pageCount: 0,
-      itemsPerPage:10
+      itemsPerPage:10,
+
+      // 참여 기록인지 진행 기록인지 판별하는 변수 참여 : true, 진행 : false
+      selected: true,
     }
   }
 };
@@ -135,6 +160,7 @@ export default {
 #history {
   background-color: #fff;
   padding: 5%;
+  padding-bottom: 2%;
   text-align: left;
   height: 100%;
   display: flex;
@@ -158,16 +184,30 @@ a {
 }
 
 #content {
-  height: 300px;
   background-color: #fff;
   padding: 2%;
+}
+
+#selectBtn {
+  background-color: #fff;
+  color: #0c0f66;
+  font-weight: 600;
+  font-size: 16px;
+}
+
+#selectedBtn {
+  background-color: #0c0f66;
+  color: #fff;
+  font-weight: 600;
+  font-size: 16px;
+}
+
+#table {
+  height: 520px;
 }
 
 button:before {
   background-color: transparent;
 }
 
-#info {
-  height: 200px;
-}
 </style>
