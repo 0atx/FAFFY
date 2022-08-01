a68d88f4f8f3ebc8807f79f930df745f0004880e
kyong0409 <kyong0409@nate.com>
2022-08-01 13:04:08 +0900

    Feat: [FE] 최신순, 답글 많은 순 정렬 기능 구현

--- frontend/src/components/board/ArticleList.vue
+++ frontend/src/components/board/ArticleList.vue
@@ -6,6 +6,9 @@
 <template>
   <div>
     <h3>{{ type }} 게시판</h3>
+    <v-btn color="success" @click="sortByDate">최신순</v-btn>
+    <v-btn color="primary" @click="sortByComments">답글 많은 순</v-btn>
+    <v-btn color="warning">글 쓰기</v-btn>
     <v-simple-table>
       <template v-slot:default>
         <thead>
@@ -15,7 +18,7 @@
             </th>
             <!--추후에 카테고리 대신 댓글 수로 대체 예정-->
             <th class="text-center">
-              카테고리
+              댓글 수
             </th>
             <th class="text-center">
               제목
@@ -34,7 +37,7 @@
             style="cursor: pointer"
           >
             <td>{{ article.writer }}</td>
-            <td>{{ article.category }}</td>
+            <td>{{ article.comments }}</td>
             <td>{{ article.title }}</td>
             <td>{{ article.created_at }}</td>
           </tr>
@@ -60,8 +63,9 @@ export default {
   data () {
       return {
         page: 1,
-        itemsPerPage: 2,
+        itemsPerPage: 5,
         totalVisible: 7,
+        now: []
       }
     },
   props: {
@@ -69,34 +73,47 @@ export default {
   },
   computed: {
     ...mapGetters(boardStore, ['freeArticles', 'qnaArticles', 'reviewArticles']),
-    nowArticles() {
-      if (this.type === '자유') {
-        return this.freeArticles
-      } else if (this.type === '질문') {
-        return this.qnaArticles
-      } else {
-        return this.reviewArticles
-      }
-    },
     totalPages() {
-      return this.nowArticles.length % this.itemsPerPage > 0 ? parseInt(this.nowArticles.length/this.itemsPerPage)+1 : parseInt(this.nowArticles.length/this.itemsPerPage)
+      return this.now.length % this.itemsPerPage > 0 ? parseInt(this.now.length/this.itemsPerPage)+1 : parseInt(this.now.length/this.itemsPerPage)
     },
     currentPage() {
       const start = (this.page-1)*this.itemsPerPage
       const end = start+this.itemsPerPage
-      return this.nowArticles.slice(start, end)
-    }
+      return this.now.slice(start, end)
+    },
   },
   methods: {
     // 상세조회 페이지 이동(추후 수정)
     articleDetail(article) {
       console.log(`제목: ${article.title} 글로 이동`)
+    },
+    // 최신순 정렬
+    sortByDate() {
+      this.now.sort((a, b) => {
+        return new Date(b.created_at) - new Date(a.created_at)
+      })
+    },
+    // 답글 많은 순 정렬
+    sortByComments() {
+      this.now.sort((a, b) => {
+        return b.comments - a.comments
+      })
     }
   },
   watch: {
     type() {
       this.page = 1
+      if (this.type === '자유') {
+        return this.now = this.freeArticles
+      } else if (this.type === '질문') {
+        return this.now = this.qnaArticles
+      } else {
+        return this.now = this.reviewArticles
+      }
     }
+  },
+  created() {
+    this.now = this.freeArticles
   }
 }
 </script>
