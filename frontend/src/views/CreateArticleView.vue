1c266891b07b81d8527c1799c108dfc8011bcd34
kyong0409 <kyong0409@nate.com>
2022-08-02 16:32:41 +0900

    Feat: [FE] 게시글 작성 기능
    게시글 작성 기능 구현입니다.
    해시태그는 ERD에 해시태그 컬럼이 없어서 확인후 추가 예정
    AxiosError 존재하는 상태. 추후 수정 예정

--- frontend/src/views/CreateArticleView.vue
+++ frontend/src/views/CreateArticleView.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container class="grey lighten-5">
-    <h1>글 작성</h1>
+    <div>
+      <h1>글 작성</h1>
+      <v-btn @click="moveToBoard">게시판으로 돌아가기</v-btn>
+    </div>
     <CreateArticleForm />
   </v-container>
 </template>
@@ -12,6 +15,11 @@ export default {
   name: 'CreateArticleView',
   components: {
     CreateArticleForm
+  },
+  methods: {
+    moveToBoard() {
+      this.$router.push({ name: 'board' })
+    }
   }
 }
 </script>
