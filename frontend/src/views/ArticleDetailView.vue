65e280e53e166ccf95e7251944a26af43f9e3a9f
kyong0409 <kyong0409@nate.com>
2022-08-03 11:46:18 +0900

    Feat: [FE] 게시글 상세보기 초기 구현
    기본정보 조회 가능,
    이미지 아직 미구현, 추후 댓글 컴포넌트 추가 예정

---
+++ frontend/src/views/ArticleDetailView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <h1>게시글 상세보기</h1>
+    <ArticleDetail />
+  </v-container>
+</template>
+
+<script>
+import ArticleDetail from '@/components/board/ArticleDetail.vue'
+
+export default {
+  name: 'ArticleDeatilView',
+  components: {
+    ArticleDetail
+  }
+}
+</script>
+
+<style>
+
+</style>
