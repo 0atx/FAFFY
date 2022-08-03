1df95260b7f4a5e588f2092775ccdd2acc70dd59
kyong0409 <kyong0409@nate.com>
2022-08-03 17:24:25 +0900

    Feat: [FE] 댓글 작성 초기 설정

--- frontend/src/components/comment/CommentForm.vue
+++ frontend/src/components/comment/CommentForm.vue
@@ -1,10 +1,29 @@
+<!--
+  작성자: 류경하
+  설명: 댓글 작성
+  최종 수정일: 2022.08.03
+-->
 <template>
-  <h1>댓글 작성</h1>
+  <v-container>
+    <v-row>
+      <v-col cols="8">
+        <v-textarea
+          clearable
+          clear-icon="mdi-close-circle"
+          label="Text"
+          value="This is clearable text.">
+        </v-textarea>
+      </v-col>
+      <v-col cols="4">
+        <v-btn color="warning" label="등 록"></v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  name: 'CommentForm',
 }
 </script>
 
