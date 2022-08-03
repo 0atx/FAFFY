ed350f8e1c3b401f0386a8ee5dfefbb9add184c9
kyong0409 <kyong0409@nate.com>
2022-08-03 15:34:53 +0900

    Fix: [FE] 게시글 조회 관련 에러 수정
    게시글 목록 조회, 상세 조회 관련 에러와 몇몇 표기를 수정하였습니다.

--- frontend/src/components/board/ArticleDetail.vue
+++ frontend/src/components/board/ArticleDetail.vue
@@ -1,8 +1,14 @@
+<!--
+  작성자: 류경하
+  설명: 게시판 글 상세조회
+  최종 수정일: 2022.08.03
+-->
 <template>
   <v-container>
     <h2>게시글 내용</h2>
     <p>제목: {{ currentArticle.title }}</p>
-    <p>작성자: {{ currentArticle.writer }}</p>
+    <p>작성자: {{ currentArticle.user.nickname }}</p>
+    <p>카테고리: {{ currentArticle.category }}</p>
     <p>조회수: {{ currentArticle.hit }}</p>
     <p>내용: {{ currentArticle.content }}</p>
   </v-container>
