f51ea9976ece3c09be75bccbeb744d93071295c9
smkim <kimsm9822@gmail.com>
2022-08-07 16:48:47 +0900

    feat: 메인 화면에 필요한 API 추가 진행 중

---
+++ backend-java/src/main/java/com/faffy/web/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.faffy.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/main")
+@CrossOrigin("*")
+public class MainController {
+
+}
