055c6aed36d3e2698299c86fdff2c11abddbec0e
Yunha <ha110011@naver.com>
2022-08-11 19:03:58 +0900

    Style: [FE] 게시판 페이지 전체 CSS 수정
    게시판 페이지 관련 레이아웃 및 CSS 수정했습니다.

--- frontend/src/components/board/CommentForm.vue
+++ frontend/src/components/board/CommentForm.vue
@@ -1,29 +1,36 @@
 <!--
   작성자: 류경하
   설명: 댓글 작성
-  최종 수정일: 2022.08.05
+  최종 수정일: 2022.08.11
+  최종 수정자: 박윤하
 -->
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="10">
-        <!-- 댓글 작성 -->
-        <v-textarea
-          clearable
-          outlined
-          clear-icon="mdi-close-circle"
-          label="Text"
-          v-model="comment"
-          hide-details
-          rows="3"
-          value="This is clearable text.">
-        </v-textarea>
-      </v-col>
-      <!-- 댓글 작성 버튼 -->
-      <v-col cols="2">
-        <v-btn id="commentBtn" height="100%" width="100%" @click="submitComment(comment)">등록</v-btn>
-      </v-col>
-    </v-row>
+  <v-container class="ml-2">
+    <div>
+      <v-row class="mx-auto" style="width:80%;">
+        <v-col class="pb-0" cols="12">
+          <!-- 댓글 작성 -->
+          <v-textarea
+            style="background-color:white"
+            outlined
+            clearable
+            clear-icon="mdi-close-circle"
+            placeholder="댓글을 입력하세요."
+            no-resize
+            color="#0c0f66"
+            v-model="comment"
+            hide-details
+            rows="3">
+          </v-textarea>
+        </v-col>
+      </v-row>
+      <v-row class="mx-auto" style="width:80%;text-align:end">
+        <!-- 댓글 작성 버튼 -->
+        <v-col cols="12">
+          <v-btn id="commentBtn" style="height:38px; width:80px;" @click="submitComment(comment)">등록</v-btn>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -47,7 +54,11 @@ export default {
     ...mapActions(boardStore, ['createComment']),
     submitComment(comment) {
       if (!this.comment) {
-        alert('내용을 입력하세요.')
+        this.$dialog.message.info('내용을 입력하세요.', {
+          position: "top",
+          timeout: 2000,
+          color: "#ff7451",
+        });
       } else {
         const commentForm = {
           board_no: this.boardNo,
@@ -68,4 +79,8 @@ export default {
   color: white;
 }
 
+::v-deep ::-webkit-scrollbar {
+  width: 0;
+  background: transparent;
+}
 </style>
