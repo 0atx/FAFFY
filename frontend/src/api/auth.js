5d23d0ab076a9035cb665589548f9109531b802f
smkim <kimsm9822@gmail.com>
2022-08-11 14:13:34 +0900

    feat: 구글 소셜 로그인 해당 jwt 토큰과 유저 정보를 메인 페이지로 리다이렉트하며 반환하는 API 구현

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -11,6 +11,14 @@ const auth = {
       .then(success)
       .catch(fail);
   },
+  socialLogin: async function (platform, success, fail){
+    await api
+      .get(
+        `/auth/login/${platform}`
+      )
+      .then(success)
+      .catch(fail);
+  },
   logout: async function(success,fail) {
     api.defaults.headers["X-AUTH-TOKEN"] =
       sessionStorage.getItem("X-AUTH-TOKEN");
