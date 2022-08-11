167fec8634e50966e6b7c884508d675344447228
Jun <js727r@gmail.com>
2022-08-11 17:12:44 +0900

    Feat: Add consulting axios
    방송 정보요청 및 참여 기록 추가요청 axios를 추가하였습니다.

--- frontend/src/api/consulting.js
+++ frontend/src/api/consulting.js
@@ -2,6 +2,14 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 const consulting = {
+  getConsulting: async function(consulting_no) {
+    return new Promise((resolve,reject)=> {
+      api.get(`/consultings/${consulting_no}`)
+      .then(response=>resolve(response.data))
+      .catch(error=>reject(error.response));
+    })
+  }
+  ,
   createConsulting: async function(consulting) {
     return new Promise((resolve,reject)=> {
       api.post(`/consultings`,JSON.stringify(consulting))
@@ -12,6 +20,13 @@ const consulting = {
   deleteConsulting: async function(request,success,fail) {
     api.delete(`/consultings`,{data:JSON.stringify(request)})
     .then(success).catch(fail);
+  },
+  createViewLog: async function(consulting) {
+    return new Promise((resolve,reject)=> {
+      api.post(`/consultings/log`,JSON.stringify(consulting))
+      .then(response=>resolve(response.data))
+      .catch(error=>reject(error.response));
+    })
   },
   uploadSnapshop : async function(formData) {
     return new Promise((resolve, reject) => {
