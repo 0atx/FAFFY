1c40764d1f2b545ee92d011292de7b28f9053ca7
smkim <kimsm9822@gmail.com>
2022-08-14 00:41:46 +0900

    feat: S07P12A802-75 S07P12A802-104 네이버 로그인 기능 구현

---
+++ frontend/src/views/handler/NaverAuthHandler.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    Please wait...
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+import { auth } from "@/api/auth";
+
+export default {
+  name: 'NaverAuthHandler',
+  created() {
+    const code = this.$route.query.code;
+    const state = this.$route.query.state;
+    if (code) {
+      auth.socialCallback(
+        'naver',
+        {code, state},
+        (response) => {
+          let token = response.data["token"];
+          let user = response.data["user"];
+          sessionStorage.setItem("X-AUTH-TOKEN", token);
+          this.SET_USER_INFO(user);
+          this.$router.push({ name: "main" });
+        },
+        () => {
+          alert("socialCallback failed");
+          this.$router.push({ name: "main" });
+        }
+      )
+    }
+    else{
+      console.log("code is null");
+    }
+  },
+  computed: {
+    ...mapState("authStore" ,["loginUser"]),
+  },
+  methods: {
+    ...mapMutations("authStore", [
+      "SET_USER_INFO"
+    ]),
+  }
+}
+</script>
+
+<style>
+
+</style>
+
