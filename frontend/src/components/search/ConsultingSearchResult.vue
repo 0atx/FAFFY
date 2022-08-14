475353a86419494c04277a7871fd66ac3a325ce1
kyong0409 <kyong0409@nate.com>
2022-08-14 17:53:07 +0900

    Feat: [FE] 방송 검색 결과 반영

--- frontend/src/components/search/ConsultingSearchResult.vue
+++ frontend/src/components/search/ConsultingSearchResult.vue
@@ -2,10 +2,10 @@
   <v-container class="d-flex flex-column">
     <v-row class="d-flex justify-space-between">
       <h2 class="ml-4">방송 검색 결과</h2>
-      <v-btn v-if="consultings.length != 0" class="sortBtn" text>인기 순</v-btn>
+      <v-btn v-if="isResultExist" class="sortBtn" text>인기 순</v-btn>
     </v-row>
     <v-row>
-      <v-col style="height:100px; display:flex; justify-content:center; align-items:center;" v-if="consultings.length == 0" cols="12">
+      <v-col style="height:100px; display:flex; justify-content:center; align-items:center;" v-if="!isResultExist" cols="12">
         <h2>검색 결과가 없습니다.</h2>
       </v-col>
       <v-col cols="3" v-for="consulting in currentPage" :key="consulting.host">
@@ -95,7 +95,7 @@
     </v-row>
 
     <!--페이지네이션-->
-    <v-row v-if="consultings.length != 0">
+    <v-row v-if="isResultExist">
       <v-col cols="12">
         <v-pagination
           v-model="page"
@@ -112,38 +112,46 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import { mapGetters } from 'vuex'
+const searchStore = "searchStore"
+
 export default {
   name: 'ConsultingSearchconsulting',
   data() {
     return {
-      consultings: [
-                { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하2', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성2', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석2', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만2', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하3', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성3', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석3', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
-        { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만3', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
-      ],
+      // consultings: [
+      //           { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하2', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성2', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석2', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만2', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '류경하3', memberLimit: 10, currentMember: 6, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '이준성3', memberLimit: 40, currentMember: 13, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김명석3', memberLimit: 27, currentMember: 14, categories: ['힙합', '빈티지'], show: false},
+      //   { no: 1, img: 'http://www.yomidog.com/preSaleUpFile/190724_%EC%84%B1%EB%B6%81%ED%8F%AC%EB%A9%94_638.JPG', title: '강아지라네', host: '김수만3', memberLimit: 33, currentMember: 33, categories: ['힙합', '빈티지'], show: false},
+      // ],
       page: 1,
       itemsPerPage: 4,
       totalVisible: 7,
     }
   },
   computed: {
+    ...mapGetters(searchStore, ['consultingResult']),
     totalPages() {
-      return this.consultings.length % this.itemsPerPage > 0 ? parseInt(this.consultings.length/this.itemsPerPage)+1 : parseInt(this.consultings.length/this.itemsPerPage)
+      return this.consultingResult.length % this.itemsPerPage > 0 ? parseInt(this.consultingResult.length/this.itemsPerPage)+1 : parseInt(this.consultingResult.length/this.itemsPerPage)
     },
     currentPage() {
       const start = (this.page-1)*this.itemsPerPage
       const end = start+this.itemsPerPage
-      return this.consultings.slice(start, end)
-    }
+      return this.consultingResult.slice(start, end)
+    },
+    isResultExist() {
+      return !_.isEmpty(this.consultingResult)
+    },
   }
 }
 </script>
