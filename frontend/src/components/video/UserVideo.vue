400ad5627a42b4ee64e3c77af176a5d5cd52492d
Jun <js727r@gmail.com>
2022-08-15 15:07:13 +0900

    Feat: 스냅샷 촬영 및 업로드 기능 추가
    스냅샷 촬영과 업로드 하는 기능을 추가했습니다.

--- frontend/src/components/video/UserVideo.vue
+++ frontend/src/components/video/UserVideo.vue
@@ -24,7 +24,6 @@
 
 <script>
 import OvVideo from './OvVideo';
-import {consulting} from '@/api/consulting.js'
 
 export default {
 	name: 'UserVideo',
@@ -47,8 +46,6 @@ export default {
 
 	methods: {
 		getConnectionData () {
-			// const { connection } = this.streamManager.stream;
-			// return JSON.parse(connection.data);
       let namecode = JSON.parse(this.streamManager.stream.connection.data).clientData.split(':');
         let user = {
           no:Number(namecode[0]),
@@ -57,67 +54,9 @@ export default {
         return user;
 		},
     capture() {
-      console.log("#######################"+this.clientData);
       var video = this.$refs.ov_video.$refs.video;
-      console.log(video);
-      // var video = document.querySelector('video');
-      var canvas = document.getElementById("drawCanvas"+this.clientData);
-      // var canvas = this.$refs.clientData;
-      console.log(canvas);
-			var context = canvas.getContext("2d");
-
-			context.drawImage(video, 0, 0, 320, 240);
-      console.log("캡쳐 완료");
-
-      console.log("#######################");
-
-
+      this.$emit('capture-event',video);
     },
-    upload() {
-      console.log("#######################");
-
-      var canvas = document.getElementById("drawCanvas"+this.clientData);
-      const imgBase64 = canvas.toDataURL('image/jpeg','multipart/form-data');
-			const decodImg = atob(imgBase64.split(',')[1]);
-
-			let array = [];
-			for (let i = 0; i < decodImg.length; i++) {
-				array.push(decodImg.charCodeAt(i));
-			}
-
-			const file = new Blob([ new Uint8Array(array) ], {
-				type : 'image/jpeg'
-			});
-			const fileName = 'web_snapshot_'+Date.now()+'.jpg';
-			let formData = new FormData();
-      const consulting_no = this.streamManager.session.sessionId;
-			formData.append('file', file, fileName);
-      formData.append('consulting_no',consulting_no);
-      console.log(formData);
-
-      consulting.uploadSnapshop(formData)
-      .then(response=> {
-        console.log(response);
-      })
-      .catch(response => {
-        console.log(response);
-      })
-
-      console.log("#######################");
-
-
-      // $.ajax({
-			// 	type : 'post',
-			// 	url : '../upload',
-			// 	cache : false,
-			// 	data : formData,
-			// 	processData : false,
-			// 	contentType : false,
-			// 	success : function(data) {
-			// 		alert('Uploaded !!');
-			// 	}
-			// });
-    }
 	},
 };
 </script>
